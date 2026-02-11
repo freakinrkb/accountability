fa210c6b171650b11d3ceaac1384e7f70f9a91f2
freakinrkb <ra7nak@gmail.com>
2026-02-11 20:48:18 +0530

    update CORS for production

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // ===== CORS =====
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
 
